2fb0b0978f8f29c4af3a48adeaa3b8c9f0fdc2b8
Goel-Akshit <ssdk171@gmail.com>
2020-04-04 22:31:40 +0530

    basic template and static files copied from previous project and routing to it set

--- Project/ME/urls.py
+++ Project/ME/urls.py
@@ -1,4 +1,4 @@
-"""Project URL Configuration
+"""Me app URL configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
+
+from . import views
 
 urlpatterns = [
 
-    path('', include('ME.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.home, name = "home-page"),
+
 ]
--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+#-------------------------------------------------------------------------------------------------
+
+    'ME.apps.MeConfig',
+#----------------------------------------------------------------------------------------------------
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
